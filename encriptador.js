58535e86164763c26a5b8a6a921ec0bfc48c48f3
CarlaAlyssa <carlyssa23@gmail.com>
2022-10-11 19:50:05 -0500

    ver fin
    ver fin

---
+++ encriptador.js
@@ -0,0 +1,66 @@
+/*Las "llaves" de encriptación que utilizaremos son las siguientes:
+
+La letra "e" es convertida para "enter"
+La letra "i" es convertida para "imes"
+La letra "a" es convertida para "ai"
+La letra "o" es convertida para "ober"
+La letra "u" es convertida para "ufat"*/
+
+var desencriptar = document.querySelector(".texto-desencriptar");
+var texto = document.querySelector(".texto-encriptar");
+
+function btnEncriptar() {
+  const textoEncriptado = encriptarTexto(texto.value);
+  desencriptar.value = textoEncriptado;
+}
+
+function btnDesencriptar() {
+  const textoDesencriptado = desencriptarTexto(texto.value);
+  desencriptar.value = textoDesencriptado;
+}
+
+function btnCopiar() {
+  desencriptar.select();
+  navigator.clipboard.writeText(desencriptar.value);
+  alert("Texto copiado!");
+}
+
+function encriptarTexto(texto) {
+  const patronE = /e/g;
+  const patronI = /i/g;
+  const patronA = /a/g;
+  const patronO = /o/g;
+  const patronU = /u/g;
+  const reemplazarEPor = "enter";
+  const reemplazarIPor = "imes";
+  const reemplazarAPor = "ai";
+  const reemplazarOPor = "ober";
+  const reemplazarUPor = "ufat";
+  texto = texto.replace(patronE, reemplazarEPor);
+  texto = texto.replace(patronI, reemplazarIPor);
+  texto = texto.replace(patronA, reemplazarAPor);
+  texto = texto.replace(patronO, reemplazarOPor);
+  texto = texto.replace(patronU, reemplazarUPor);
+  var textoL = document.querySelector(".texto-encriptar");
+  textoL.value = "";
+  return texto;
+}
+
+function desencriptarTexto(texto) {
+  const patronE = /enter/g;
+  const patronI = /imes/g;
+  const patronA = /ai/g;
+  const patronO = /ober/g;
+  const patronU = /ufat/g;
+  const reemplazarEPor = "e";
+  const reemplazarIPor = "i";
+  const reemplazarAPor = "a";
+  const reemplazarOPor = "o";
+  const reemplazarUPor = "u";
+  texto = texto.replace(patronE, reemplazarEPor);
+  texto = texto.replace(patronI, reemplazarIPor);
+  texto = texto.replace(patronA, reemplazarAPor);
+  texto = texto.replace(patronO, reemplazarOPor);
+  texto = texto.replace(patronU, reemplazarUPor);
+  return texto;
+}
